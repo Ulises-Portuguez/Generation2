4c44fc1633699af5a15a324e0e7c372c7f20a602
Ulises-Portuguez <84081256+Ulises-Portuguez@users.noreply.github.com>
2022-01-18 20:43:31 -0600

    Ejercicio de bucles 17 de enero

---
+++ javascript/bucles/bucles.js
@@ -0,0 +1,66 @@
+//Exercise 1
+
+//Part One
+// var people = ["Sofía", "David", "Juan"];
+
+// console.log(people);
+
+
+//Part 2
+
+// people.push("Sara", "Agustin");
+// people.shift();
+// console.log(people);
+
+// people.splice(1, 0, "Renata");
+// people.push("Elena");
+// console.log(people);
+
+// Exercise 2
+// var figura = ""
+// for (let i = 0; i <= 4; i++) {
+//     figura = figura + "*"
+//     console.log(figura);
+// }
+
+//Exersice 3
+// var xValue = parseInt(prompt("Introduce un valor entero"));
+// while (xValue >= 0.5) {
+//     xValue = xValue - 0.5;
+//     console.log(xValue);
+// }
+
+// {
+//     let i = 1
+//     while (i <= 98) {
+
+//         console.log(i)
+//         i += 2
+//     }
+// }
+
+var n = 6;
+var i = 0;
+while (i <= 6) {
+    console.log(`[${i}]`)
+    i++
+}
+
+var w = 1;
+let m = '';
+while (w <= 6) {
+    m = m + "[" + w + "]";
+    w++;
+}
+console.log(m)
+
+// {
+//     let suma = 0;
+//     let n = 21;
+//     let i = 0;
+//     while (i < n) {
+//         i++;
+//         suma += i;
+//     }
+//     console.log(suma);
+// }
